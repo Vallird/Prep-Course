482b46bd0edb0583fd16657751edd8f484615ce6
Vallird <nahuel00senn@gmail.com>
2022-05-04 17:49:00 -0300

    quinto commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -165,40 +165,49 @@ function esPositivo(numero) {
   //Si el número es positivo, devolver ---> "Es positivo"
   //Si el número es negativo, devolver ---> "Es negativo"
   //Si el número es 0, devuelve false
-  
+  if(numero===0){
+    return false;
+  }
+  if(numero>0){
+    return 'Es positivo';
+  }
+  else{
+    return 'Es negativo';
+  }
 }
 
 function agregarSimboloExclamacion(str) {
   // Agrega un símbolo de exclamación al final de la string "str" y devuelve una nueva string
   // Ejemplo: "hello world" pasaría a ser "hello world!"
   // Tu código:
+  return str + '!';
 }
 
 function combinarNombres(nombre, apellido) {
   // Devuelve "nombre" y "apellido" combinados en una string y separados por un espacio.
   // Ejemplo: "Soy", "Henry" -> "Soy Henry"
   // Tu código:
-  
+  return nombre + ' ' + apellido;
 }
 
 function obtenerSaludo(nombre) {
   // Toma la string "nombre" y concatena otras string en la cadena para que tome la siguiente forma:
   // "Martin" -> "Hola Martin!"
   // Tu código:
-  
+  return 'Hola ' + nombre + '!';
 }
 
 function obtenerAreaRectangulo(alto, ancho) {
   // Retornar el area de un rectángulo teniendo su altura y ancho
   // Tu código:
-  
+  return alto*ancho;
 }
 
 
 function retornarPerimetro(lado){
   //Escibe una función a la cual reciba el valor del lado de un cuadrado y retorne su perímetro.
   //Escribe tu código aquí
-  return lado * lado;
+  return lado*4;
 }
 
 
@@ -213,7 +222,7 @@ function deEuroAdolar(euro){
   //Supongamos que 1 euro equivale a 1.20 dólares. Escribe un programa que reciba
   //como parámetro un número de euros y calcule el cambio en dólares.
   //Escribe tu código aquí
-  
+  return euro*1.20;
 }
 
 
@@ -223,17 +232,13 @@ function esVocal(letra){
   //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
   //Escribe tu código aquí
-  if(esVocal.length !== 1){
-    return 'Dato incorrecto';
+  if(letra.length > 1){
+    return "Dato incorrecto";
   }
-    if(esVocal === 'a'||'e'||'i'||'o'||'u'){
-      return 'Es vocal';
+  if(letra === "a" || letra === "e" || letra === "i" || letra === "o" || letra === "u"){
+    return "Es vocal";
   }
-  else{
-    return 'Dato incorrecto';
-  }
-
-  
+  return "Dato incorrecto";
 }
 
 
--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -225,7 +225,25 @@ function breakStatement(numero) {
   //devolver: "Se interrumpió la ejecución"
   //Pista: usá el statement 'break'
   // Tu código:
-  
+  var nuevoArray = [];
+var suma = numero;
+
+for(var i=0; i<10; i++){
+  suma = suma + 2;
+
+  if(suma === i){
+    break;
+  }
+  else{
+    nuevoArray.push(suma);
+  }
+}
+if(i < 10){
+  return 'Se interrumpió la ejecución';
+}
+else{
+  return nuevoArray;
+}
 }
 
 
@@ -236,6 +254,22 @@ function continueStatement(numero) {
   //Cuando el número de iteraciones alcance el valor 5, no se suma en ese caso y se continua con la siguiente iteración
   //Pista: usá el statement 'continue'
   // Tu código:
+  var array = [];
+var suma = numero;
+
+for(var i=0; i<10;i++){
+
+  if(i === 5){
+    continue;
+  }
+  else{
+    suma = suma + 2;
+    array.push(suma);
+  }
+
+
+}
+return array;
 }
 
 
